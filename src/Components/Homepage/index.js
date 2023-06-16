c84465acde227c95b8ee03808d931a301633063f
PrimoPan <898021802@qq.com>
2023-06-16 18:02:12 +0800

    updatepng

--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import {Image} from "react-bootstrap";
+import logo from "../../img/logo.png"
+
 
 function Homepage()
 {
     return (
-        <Container>
+        <Container className="vh-100"  fluid style={{background:"linear-gradient(white, black)"}}>
+            <Row>
+                <Col xs={7}>
+                    <p >
+                        Sparkle
+                    </p>
+                </Col>
+                 <Col>
+                        <br/>
+                        <br/>
+                        <br/>
+                     <br/>
+                     <br/>
+                     <br/>
+                      <img src={logo} alt="logo" />
+                 </Col>
+                <Col>
 
+                </Col>
+            </Row>
         </Container>
 )
 }
